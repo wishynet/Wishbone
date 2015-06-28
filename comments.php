15facbbd214f4443e0b3e00ac6963c9289503bd1
wishypw <wishypw@gmail.com>
2015-06-28 10:41:44 +0100

    Update Login Section
    Add logout link to the login section in the header when a user is
    logged in.
    
    Add ‘display: block’ to omg elements so that descending space is
    removed.

--- comments.php
+++ comments.php
@@ -1,7 +1,8 @@
 <hr>
 
 <!-- if the post is password protected, don't load any comments elements -->
-<?php if ( post_password_required() ) {
+<?php if ( post_password_required() )
+{
 	return;
 }; ?>
 
@@ -11,15 +12,15 @@
 
 <!-- if there are any comments, then do the following... -->
 <?php if ( have_comments() ) : ?>
-	
+
 
 <!-- comment count -->
 <p id="comment-count">( <?php printf( _n( 'One Comment', '%1$s Comments', get_comments_number() ), number_format_i18n( get_comments_number() ) ); ?> )</p>
-	
-	
+
+
 <!-- comment loop -->
 <?php $args = array(
-	
+
 	'style'             => 'div',
 	'callback'          => null,
 	'end-callback'      => null,
@@ -28,44 +29,44 @@
 	'page'              => null,
 	'per_page'          => '',
 	'avatar_size'       => 96,
-				
+
 ); ?>
 
 <?php wp_list_comments( $args ); ?>
 
 
 <!-- comment pagination -->
-<div class="comment-nav">	
-	
+<div class="comment-nav">
+
 	<?php paginate_comments_links(); ?>
-	
+
 </div><!-- end of comment-nav -->
 
 
 <!-- alternate responses -->
 <?php else: ?>
-	
+
 	<p><?php _e( 'There are no Comments', 'wishbone' ); ?></p>
-	
+
 <?php endif; ?>
 
 
 <?php if ( !comments_open() ) : ?>
 
 	<h3><?php _e( 'Comments are Closed', 'wishbone' ); ?></h3>
-	
+
 <?php else: ?>
-	
+
 
 <!-- comment form -->
 <?php $args = array(
-	
+
 	'title_reply' 	=> __( 'Leave a Comment:', 'wishbone' ),
 	'label_submit' 	=> __( 'Add Your Comment', 'wishbone' )
-	
+
 ); ?>
-		
+
 <?php comment_form( $args ); ?>
-		
-	
+
+
 <?php endif; ?><!-- end 'if' have comments -->
--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 
 <html <?php language_attributes(); ?>>
-	
+
 <head>
 
 	<!-- Meta Data -->
@@ -13,76 +13,93 @@
 
 	<!-- The wp_head hook -->
 	<?php wp_head(); ?>
-	
+
 </head>
 
 
 <body <?php body_class(); ?>>
-	
+
 
 	<?php if( get_theme_mod( 'wishbone_toggle_header_setting', true ) != '' ) { ?>
 
 	<div id="header">
-	
+
 	<div class="container">
-	
+
 
 		<div class="sixteen columns">
-		
+
 			<?php if ( get_theme_mod( 'wishbone_header_toggle_logo_setting', true ) ) : ?>
-	
+
 				<div id="logo">
-					
+
 					<?php if ( get_theme_mod( 'wishbone_logo_setting', false ) ) : ?>
-		
+
 						<img src="<?php echo get_theme_mod( 'wishbone_logo_setting' ); ?>" alt="<?php _e( 'logo', 'wishbone' ); ?>" />
-					
+
 					<?php else : ?>
-						
+
 						<img src="<?php echo get_template_directory_uri(); ?>/images/wishbone.png" alt="<?php _e( 'logo', 'wishbone' ); ?>" />
-					
+
 					<?php endif; ?>
-	
+
 				</div><!-- end of logo -->
-		
+
 			<?php endif; ?>
 
-			
+
 			<?php if ( get_header_textcolor() != 'blank' ) : ?>
-		
+
 			<div id="title">
-		
+
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
 					<h1><?php bloginfo( 'name' ); ?></h1>
 				</a>
-		
+
 				<p><?php bloginfo( 'description' ); ?></p>
-				
+
 			</div><!-- end of title -->
-		
+
 			<?php endif; ?>
-		
-		
+
+
 			<?php if ( get_theme_mod( 'wishbone_header_toggle_login_setting', true ) ) : ?>
-	
+
 			<div id="login">
-			
+
+				<?php if ( is_user_logged_in() ) : ?>
+
+				<ul>
+					<li class="button">
+						<?php
+							$current_user = wp_get_current_user();
+							echo 'Hi there, ' . $current_user->user_login;
+						?>
+					</li>
+				</ul>
+
+				<a href="<?php echo esc_url( wp_logout_url() ); ?>" title="<?php _e( 'Logout', 'wishbone' ); ?>"><?php _e( 'Logout', 'wishbone' ); ?></a>
+
+				<?php else : ?>
+
 				<ul>
 					<li><a href="<?php echo esc_url( wp_login_url() ); ?>" class="button"><?php _e( 'Login', 'wishbone' ); ?></a></li>
 					<li><a href="<?php echo esc_url( wp_registration_url() ); ?>" class="button"><?php _e( 'Sign Up', 'wishbone' ); ?></a></li>
 				</ul>
-			
+
 				<a href="<?php echo esc_url( wp_lostpassword_url() ); ?>" title="<?php _e( 'Password Lost and Found', 'wishbone' ); ?>"><?php _e( 'Lost your password?', 'wishbone' ); ?></a>
-				
+
+				<?php endif; ?>
+
 			</div><!-- end of login -->
-		
+
 			<?php endif; ?>
 
 		</div><!-- end of sixteen columns -->
-	
-		
+
+
 	</div><!-- container -->
-	
+
 	</div><!-- end of header -->
 
 	<?php } /* end of wishbone_toggle_header_setting */ ?>
@@ -92,55 +109,55 @@
 	<?php if( get_theme_mod( 'wishbone_toggle_navmenu_setting', true ) != '' ) { ?>
 
 	<div id="navigation">
-		
+
 	<div class="container">
-	
-		
+
+
 		<div class="sixteen columns">
-		
+
 			<div id="mobile_menu">
-			
+
 				<img src="<?php echo get_template_directory_uri(); ?>/images/menu.png" alt="menu" />
-			
+
 				<p><?php _e( 'Menu', 'wishbone'); ?></p>
-		
+
 				<ul class="menu">
-					
+
 					<?php if ( get_theme_mod( 'wishbone_logo_setting', true ) ) : ?>
-					
+
 					<li class="menulogo">
-						
+
 						<img src="<?php echo get_theme_mod( 'wishbone_logo_setting' ); ?>" alt="<?php _e( 'logo', 'wishbone' ); ?>" />
 
 					</li>
-					
+
 					<?php endif; ?>
-					
+
 					<li class="menutitle"><h2><?php bloginfo( 'name' ); ?></h2></li>
-					
+
 					<hr>
-					
+
 					<li class="menulist"><?php wp_nav_menu( 'theme_location=mobile_menu' ); ?></li>
-					
+
 					<hr>
-					
+
 					<li><p><?php _e( 'Thanks for Visiting', 'wishbone' ); ?></p></li>
-					
-				</ul><!-- end of menu -->				
-		
+
+				</ul><!-- end of menu -->
+
 			</div><!-- end of mobile_menu -->
-		
+
 			<div id="desktop_menu">
-		
+
 				<?php wp_nav_menu( 'theme_location=desktop_menu' ); ?>
-			
+
 			</div><!-- end of desktop_menu -->
-		
+
 		</div><!-- end of sixteen columns -->
-	
+
 
 	</div><!-- container -->
-	
+
 	</div><!-- end of navigation -->
 
 	<?php } /* end of wishbone_toggle_navmenu_setting */ ?>
@@ -148,8 +165,7 @@
 
 	<!-- back to top button -->
 	<a href="#back-to-top" id="back-to-top">
-		
-		<img src="<?php echo get_template_directory_uri(); ?>/images/backtotop.png" alt="backtotop" />
-		
-	</a>
 
+		<img src="<?php echo get_template_directory_uri(); ?>/images/backtotop.png" alt="backtotop" />
+
+	</a>
